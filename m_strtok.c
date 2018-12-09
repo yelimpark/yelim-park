957c208a96cd97fbd52848b55e9898c5988df06d
yelimpark <43175834+yelimpark@users.noreply.github.com>
2018-12-09 22:22:54 +0900

    Create m_strtok.c

---
+++ m_strtok.c
@@ -0,0 +1,51 @@
+ #include <stdio.h>   
+ #include <string.h>
+ 
+ int main( ) 
+{ 
+   char *token = NULL; 
+   char str1[] = "This,is,string;split,Test;Program!!!! end\n"; 
+   char str2[] = ",;!"; 
+
+   token = m_strtok( str1, str2 ); 
+
+   while( token != NULL ) 
+   { 
+     printf( "token = %s\n", token ); 
+     token = m_strtok( NULL, str2 ); 
+  } 
+
+  return 0; 
+}
+
+char * m_strtok(char *str, char *tok)
+{
+   char *strReturn = str, *tokTemp = tok;
+   static char *strTemp;
+   
+   if (str != NULL) 
+       strTemp = str;
+   else
+       strReturn= strTemp;
+       
+   if (*strTemp == '\0') 
+       return NULL;
+
+   while(*strTemp != '\0')
+   {
+        tok = tokTemp;
+        while(*tok != '\0')   
+        {
+            if(*strTemp == *tok) 
+            {
+                *strTemp = '\0';
+                strTemp++;
+                return strReturn;
+            }
+            tok++;
+        }
+        strTemp ++;
+   }
+   
+   return strReturn;
+}
