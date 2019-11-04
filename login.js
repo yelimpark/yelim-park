dfc860b618cec456049b061a676c0811d812eb09
yelimpark <pyl9983@naver.com>
2019-11-04 23:57:14 +0900

    commit

--- login.js
+++
@@ -1,9 +0,0 @@
-var name
-
-function keydown() {
-	// when you press enter contents of the input will save in name
-  if (window.event.keyCode == 13) {
-      name = document.getElementById("nickname").value; 
-      location.replace("room.html?nick_name=<%=name%>);
-  }
-}
