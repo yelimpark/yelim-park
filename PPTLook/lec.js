7bf2f56b3e29224c32475e4f9a3bec8cce16798a
yelimpark <pyl9983@naver.com>
2019-11-07 21:49:20 +0900

    change

--- PPTLook/lec.js
+++ PPTLook/lec.js
@@ -1,6 +1,6 @@
 var INDENT = "    ";
-var DRAWING = false;
 
+var OBO = false;
 var smax = 1;
 var snum = 0;
 
@@ -38,11 +38,13 @@ function editHtml() {
   var layoutDiv = document.getElementsByClassName("layout");
   var layoutArr = (layoutDiv[0].innerHTML).split(/\n/g);
   layoutDiv[0].innerHTML = '<div id="side">' +
-   layoutArr[1] + layoutArr[4] + layoutArr[5] + layoutArr[6] +layoutArr[7] +
-   '</div> <div id="fold"> <a href "javascript:fold();">+</a> </div>';
+   layoutArr[1] +
+   '<div id="navList"><ul id="index" ><\/ul><\/div>' +
+   layoutArr[4] + layoutArr[5] + layoutArr[6] +layoutArr[7] +
+   '</div> <a href="javascript:sideFold()"> <div id="fold"><p>&rsaquo;</p></div> </a>';
 }
 
-//var slideIndexMap = [];
+var slideIndexMap = [];
 
 function slideLabel() {
     var slideColl = GetElementsWithClassName('*', 'slide');
@@ -74,14 +76,15 @@ function slideLabel() {
             value = n + " : === " + otext + " ===";
         }
 
+        var li = document.createElement("li");
+        li.innerText = value;
+
         var link = document.createElement("a");
         link.href = '#slide' + n;
-        link.innerText = value;
+        link.appendChild(li);
 
-        var li = document.createElement("li");
-        li.appendChild(link);
-        index.appendChild(li);
-//        slideIndexMap[n] = n;
+        index.appendChild(link);
+        slideIndexMap[n] = n;
       }
 }
 
@@ -89,23 +92,42 @@ function createControls() {
     var controlsDiv = document.getElementById("controls");
     if (!controlsDiv) return;
 
-    controlsDiv.innerHTML = '<form action="#" id="controlForm">' +
+    controlsDiv.innerHTML = //'<form action="#" id="controlForm">' +
     '<div id="navaLinks">' +
     '<a accesskey="z" id="prev" title="Previous Slide" href="javascript:go(-1);">&lsaquo;<\/a>' +
     '<span id="slideNum"> </span>' +
     '<a accesskey="x" id="next" title="Next Slide" href="javascript:go(1);">&rsaquo;<\/a></div>' +
-    '<div id="navList"><ul id="index" ><\/ul><\/div>' +
-    '<\/form>';
+    '<div id = "buttons">' +
+    '<a href="javascript:customize();" /> <img src = "images/font.png"/> </a>' +
+    '<a href="javascript:oneByOne(0);" /> <img src = "images/vertical.png"/> </a></div>';
+    //+ '<\/form>';
 }
 
-
-function fold(){
-    var sl = document.getElementById("slide");
-    if(sl.style.display=='none'){
-        sl.style.display = 'block';
+function fold(id){
+    var elem = document.getElementById(id);
+    if(elem.style.display=='none'){
+      elem.style.display = 'block';
     }else{
-        sl.style.display = 'none';
+      elem.style.display = 'none';
     }
+}
+
+function sideFold() {
+  fold('side');
+  pptResizing();
+}
+
+function pptResizing() {
+  var ppt = document.getElementsByClassName("presentation");
+  var side = document.getElementById("side");
+  if(side.style.display=='none') {
+    ppt[0].style.marginLeft = 10 + '%';
+    ppt[0].style.width = 90 + '%';
+  }
+  else {
+    ppt[0].style.marginLeft = 30 + '%';
+    ppt[0].style.width = 70 + '%';
+  }
 }
 
 function currentSlide() {
@@ -122,15 +144,41 @@ function currentSlide() {
     }
 }
 
-function sideResizing() {
-  var lDiv = document.getElementsByClassName("layout");
-  lDiv[0].style.width = (window.innerWidth * (3/10)) + 'px';
+function oneByOne(num) {
+  var slide = document.getElementsByClassName("slide");
+  if (OBO == true) {
+    for (var i=0;i<slideIndexMap.length;i++) {
+      if (i != num) {
+        slide[i].style.display = 'block';
+      }
+      else {
+        slide[i].style.fontSize = '13pt';
+      }
+    }
+    OBO = false;
+    return ;
+  }
+  for (var i=0;i<slideIndexMap.length;i++) {
+    if (i != num) {
+      slide[i].style.display = 'none';
+    }
+    else {
+      slide[i].style.fontSize = '17pt';
+    }
+  }
+  OBO = true;
+}
+
+function layoutResizing() {
+  var lOut = document.getElementsByClassName("layout");
+  lOut[0].style.height = window.innerHeight + 'px';
+  document.getElementById('index').style.height = (window.innerHeight * 0.65) + 'px';
 }
 
 function detectResize() {
   jQuery(function($){
-    $(window).resize( sideResizing );
-    sideResizing();
+    $(window).resize( layoutResizing );
+    layoutResizing();
   });
 }
 
@@ -140,6 +188,7 @@ function startup() {
   slideLabel();
   currentSlide();
   detectResize();
+  pptResizing();
 }
 
 window.onload = startup;
